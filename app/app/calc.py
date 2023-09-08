2e420a2ffe27b6092db3339f502758be7ebeb453
YourName <o.alahdab81@gmail.com>
2023-09-08 12:27:29 +0300

    installed postgresql adapter... and runned 2 tests

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calc function
+"""
+
+
+def add(x, y):
+    """Add x and y return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
