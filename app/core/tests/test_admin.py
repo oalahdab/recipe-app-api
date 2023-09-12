5a2f2d7d16e1b755a46a5f6c70f954a4445d703e
YourName <o.alahdab81@gmail.com>
2023-09-12 16:43:59 +0300

    implementing swagger and openapi

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,14 +1,14 @@
 """
-Test for the Django admin modifications.
+Tests for the Django admin modifications.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.test import Client
 
 
-
-class AdminSitetests(TestCase):
-    """"Tests for Django admin."""
+class AdminSiteTests(TestCase):
+    """Tests for Django admin."""
 
     def setUp(self):
         """Create user and client."""
@@ -23,11 +23,25 @@ class AdminSitetests(TestCase):
             password='testpass123',
             name='Test User'
         )
-    
-    def test_users_list(self):
+
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def    test_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
